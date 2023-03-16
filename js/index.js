31c2be4bb4007d780ce391fb6847c98c7956a2cd
Leonardo Pereira da Silva <leonardop.silva1622@gmail.com>
2023-03-15 21:55:29 -0300

    QTS - LIÇÃO 1
    add - os calculos

--- js/index.js
+++ js/index.js
@@ -3,20 +3,42 @@ class Pessoa {
         this.nome = nome;
         this.cpf = cpf;
         this.salarioAnual = salarioAnual;
+        this.taxa = 0;
+        this.parcela = 0;
+    }
 
+    calcularAliquota(){
+        if(this.salarioAnual <= 22847.46){
+            this.taxa = 0;
+            
+        }else if(this.salarioAnual >= 22847.46 && salarioAnual <= 33919.80){
+            this.taxa = 7.5
+        }else if(this.salarioAnual >= 33919.80 && salarioAnual <= 45012.60){
+            this.taxa = 15
+        }else if(this.salarioAnual >= 45012.60 && salarioAnual <= 55976.16){
+            this.taxa = 22.5
+        }else if(this.salarioAnual > 55976.16){
+            this.taxa = 27.5
+        }
+    }
 
+    calculoParcela(){
+        this.parcela = this.salarioAnual*(this.taxa/100);
+        this.parcela = this.parcela.toFixed(2);
     }
 
 }
 const urlParams = new URLSearchParams(window.location.search);
 
-var nome = urlParams.get("#nome") 
-var cpf = urlParams.get("#cpf") 
-var salarioAnual = urlParams.get("#salario") 
+var nome = urlParams.get("nome"); 
+var cpf = urlParams.get("cpf"); 
+var salarioAnual = urlParams.get("salario");
 
-console.log(nome)
+var pessoa = new Pessoa(nome,cpf,salarioAnual);
 
+pessoa.calcularAliquota();
+pessoa.calculoParcela();
+console.log(pessoa);
 
-console.log(nome.value)
-console.log(cpf.value)
-console.log(salarioAnual.value)
+document.getElementById('resultado').innerHTML = 'banana';
+
